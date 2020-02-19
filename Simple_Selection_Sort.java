b59d7ebe594a756bbbf06c00a756a3dfef1fb581
Musfiqur Rahman Anik <31827927+MushfiqurRahmanAnik@users.noreply.github.com>
2020-02-20 02:26:32 +0600

    Update Simple_Selection_Sort.java

--- Simple_Selection_Sort.java
+++ Simple_Selection_Sort.java
@@ -14,6 +14,9 @@ public class Node {
 }
 
 //Selection Sort Starts here
+
+//Selection Sort using list
+
 public class SelectionSort {
   public static void main(String[] args) {
     int a[]={8,58,12,54,02,641,20,4};
@@ -45,5 +48,40 @@ public class SelectionSort {
     x.elem=max;
     z.elem=temp;
     SS(n,i-1);
+  }
+}
+
+
+//Selection Sort using array
+
+public class SelectionSort
+{
+  public static void main(String[] args) 
+  {
+    int a[] ={75,7,1,9,4,7,2,5};
+    int i=a.length-1;
+    SS(a,i);
+    for(int k=0;k<a.length;k++)
+    {
+      System.out.println(a[k]);
+    }
+  }
+  public static void SS(int[] a,int i)
+  {
+    if(i==0) return;
+    int max=a[0];
+    int maxIdx=0;
+    for(int j=1;j<=i;j++)
+    {
+      if(a[j]>max)
+      {
+        max=a[j];
+        maxIdx=j;
+      }
+    }
+    int temp=a[i];
+    a[i]=max;
+    a[maxIdx]=temp;
+    SS(a,i-1);
   }
 }
